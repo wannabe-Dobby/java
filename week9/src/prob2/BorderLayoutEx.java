22affca2497bb468294bfb37160f848cdb29f01a
macbookair <macbookair@localhost>
2024-05-03 02:27:14 +0900

    .

--- week9/src/prob2/BorderLayoutEx.java
+++ week9/src/prob2/BorderLayoutEx.java
@@ -11,11 +11,11 @@ public class BorderLayoutEx extends JFrame {
 		
 		c.setLayout(new BorderLayout(30, 20));
 		
-		c.add(new JButton("Calculate"));
-		c.add(new JButton("add"));
-		c.add(new JButton("sub"));
-		c.add(new JButton("mul"));
-		c.add(new JButton("div"));
+		c.add(new JButton("Calculate"), BorderLayout.CENTER);
+		c.add(new JButton("add"), BorderLayout.NORTH);
+		c.add(new JButton("sub"), BorderLayout.SOUTH);
+		c.add(new JButton("mul"), BorderLayout.EAST);
+		c.add(new JButton("div"), BorderLayout.WEST);
 		
 		setSize(300, 200);
 		setVisible(true);
